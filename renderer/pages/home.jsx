103fa0976095bac6ca853b77124f35c867907942
Lacy Morrow <me@lacymorrow.com>
2019-11-18 20:08:03 -0800

    properly unmount

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,22 +1,34 @@
-import {ipcRenderer} from 'electron';
-import React from 'react';
+import { ipcRenderer } from 'electron';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
 const Home = () => {
 	// Because of SSR, we have to check for the existence of ipcRenderer before use
 
-	// Allow renderer process to receive communication
-	ipcRenderer && ipcRenderer.on('to-renderer', (event, arg) => {
-		console.log(arg)
-		alert(arg)
-	});
-
-
 	const handleImgClick = () => {
 		console.log('messaging worker...')
 		ipcRenderer && ipcRenderer.send('for-worker', 'message to worker');
 	}
+	useEffect(() => {
+		// componentDidMount()
+		if (ipcRenderer) {
+			// Allow renderer process to receive communication
+			ipcRenderer.on('to-renderer', (event, arg) => {
+				console.log(arg)
+				alert(arg)
+			});
+
+		}
+
+		return () => {
+			// componentWillUnmount()
+			if (ipcRenderer) {
+				// unregister it
+				ipcRenderer.removeAllListeners('to-renderer');
+			}
+		};
+	}, []);
 
   return (
     <React.Fragment>
